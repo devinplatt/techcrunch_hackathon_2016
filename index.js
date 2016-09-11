c811a1865bf3748869a1a2206044efdbe47a772b
adevbungert <antoinebungert7@gmail.com>
2016-09-11 04:35:22 -0700

    fg

--- index.js
+++ index.js
@@ -532,7 +532,7 @@ function sendRestaurantMessage(recipientId, messageText) {
   console.log(have_location);
   if (have_cuisine && have_location && have_time) {
 	  sendMessageToUserFromYelpResult(recipientId);
-  } else if (!have_cuisine || !have_location){
+  } else if (!have_cuisine || !have_location) {
     if (!have_cuisine) {
       restaurantMessageText = "What type of food would you like to eat? (eg. Mexican food).";
     } else if (!have_location) {
